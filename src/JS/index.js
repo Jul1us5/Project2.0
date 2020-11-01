2efb20322ee13b816091a81cc5b0b2abe6b268e8
Julius <Julius>
2020-11-01 20:05:20 +0200

    refactoring

---
+++ src/JS/index.js
@@ -0,0 +1,7 @@
+import data from './data'
+import '../CSS/main.css'
+
+console.log(data);
+console.log('------');
+console.log('index');
+console.log('------');
--- src/data.js
+++
@@ -1,5 +0,0 @@
-const data = {
-    title: 'My first work with Webpack!!!'
-}
-
-export default data;
