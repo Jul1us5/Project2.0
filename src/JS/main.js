6fd52d11fc3286b0b1d507fc34178f1b10c551ff
Julius <Julius>
2020-11-02 14:11:03 +0200

    Normalize.css

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,7 +1,6 @@
 import data from './data'
 import '../CSS/main.css'
 import json from '../JSON/json'
-// import webpack from '../IMG/webpack.png'
 
 const main = document.querySelector('.main')
 main.insertAdjacentHTML('afterbegin', data.title);
