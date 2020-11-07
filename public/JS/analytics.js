28958a44ac9935030ed54a0a2ec8be662e7a3c58
Julius <Julius>
2020-11-07 17:02:09 +0200

    TypeScript

--- public/JS/analytics.js
+++ public/JS/analytics.js
@@ -8,10 +8,10 @@
  */
 /******/ (() => { // webpackBootstrap
 /*!*************************!*\
-  !*** ./JS/analytics.js ***!
+  !*** ./JS/analytics.ts ***!
   \*************************/
 /*! unknown exports (runtime-defined) */
 /*! runtime requirements:  */
-eval("function createAnalytics() {\n  var counter = 0;\n  var isDestroyed = false;\n\n  var listener = function listener() {\n    return counter++;\n  };\n\n  document.addEventListener('click', listener);\n  return {\n    destroy: function destroy() {\n      document.removeEventListener('click', listener);\n      isDestroyed = true;\n    },\n    getClick: function getClick() {\n      if (isDestroyed) {\n        return 'Analytics is Destroyed!!!';\n      }\n\n      return counter;\n    }\n  };\n}\n\nwindow.analytics = createAnalytics();\n\n//# sourceURL=webpack:///./JS/analytics.js?");
+eval("function createAnalytics() {\n  var counter = 0;\n  var isDestroyed = false;\n\n  var listener = function listener() {\n    return counter++;\n  };\n\n  document.addEventListener('click', listener);\n  return {\n    destroy: function destroy() {\n      document.removeEventListener('click', listener);\n      isDestroyed = true;\n    },\n    getClick: function getClick() {\n      if (isDestroyed) {\n        return 'Analytics is Destroyed!!!';\n      }\n\n      return counter;\n    }\n  };\n}\n\nwindow['analytics'] = createAnalytics();\n\n//# sourceURL=webpack:///./JS/analytics.ts?");
 /******/ })()
 ;
